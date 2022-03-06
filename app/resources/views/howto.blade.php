b3ed03f0b23d7a87b2bc4d24c677ee3f71716323
寺下大典 <ekusiad.tera@icloud.com>
2022-03-06 19:08:38 +0900

    使い方ページ修正

--- app/resources/views/howto.blade.php
+++ app/resources/views/howto.blade.php
@@ -9,7 +9,7 @@
     <div class="howto-innner">
         <h2>①『追加する』ボタンを押す</h2>
         <img src="{{ asset('images/howto01.png') }}" alt="">
-        <h2>②『タイトル』『鑑賞日』『サムネイル』を入力する</h2>
+        <h2>②『タイトル』『鑑賞日』『サムネイル』『星評価』を入力する</h2>
         <img src="{{ asset('images/howto02.png') }}" alt="">
     </div><!-- howto-innner -->
     <a href="{{ route('index') }}" class='btn btn-info btn-back'>さっそく使ってみる</a>
